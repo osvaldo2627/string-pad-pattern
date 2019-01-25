111495224abfc14c9724d1c22c32eb0af6503e39
Osvaldo Aguilar <osvaldo2627@gmail.com>
2019-01-25 13:31:54 +0100

    Removing comments from test file

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 
 
-/*const { padPattern } = require("./index");
+const { padPattern } = require("./index");
 const testStringArray = [ "file-1.js", "file-100.js", "file-11.js", "other_string" ]
     ,pattern = "file-(?var).js" //string pattern for example "salary_(?var)_group_a"
-    ,padLength = 5             //expected lengh of the paded region specified in the pattern
+    ,padLength = 5              //expected lengh of the paded region specified in the pattern using (?var)
     ,padString = '0'            //character or characters to pad the string
     ,padRight = false;          //apply padding to the right, defualt to false
 
-const expectedResult = [ 'file-00001.js','file-00100.js','file-00011.js','other_1string' ];
+const expectedResult = [ 'file-00001.js','file-00100.js','file-00011.js','other_string' ];
 
 let testResultArray = testStringArray.map( item =>{
     return padPattern( item, pattern, padLength, padString );
@@ -15,20 +15,7 @@ let testResultArray = testStringArray.map( item =>{
 
 
 JSON.stringify(testResultArray)===JSON.stringify(expectedResult)  ?
-                                 console.log('TEST SUCCESS') : console.log('TEST FAIL');*/
+                                 console.log('TEST SUCCESS') : console.log('TEST FAIL');
 
 
-
-                                 const stringPadder = require("./index");
-
-const testString = "file-1.js"
-    ,pattern = "file-(?var).js" //string pattern for example "salary_(?var)_group_a"
-    ,padLength = 5             //expected lengh of the paded region specified in the pattern
-    ,padString = '0'            //character or characters to pad the string
-    ,padRight = false;          //apply padding to the right, defualt to false
-
-let result = stringPadder.padPattern(testString, pattern, padLength, padString);
-
-console.log(result);
-//should print file-0001.js
 
